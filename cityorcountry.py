ceb23eea2c10a6666f62c38ffcc737cf6015006d
Rosi Szele <rkimball145@gmail.com>
2021-05-12 20:10:45 +0000

    first save

---
+++ cityorcountry.py
@@ -0,0 +1,38 @@
+city= ["smog", "crowds", "fantastic restaurants", "public transportation", "sightseeing"]
+
+country= ["fresh air", "space", "bigger homes", "restaurants", "wild life", "stinky smells", "less hip dressers"]
+
+print("How do you like your air?")
+choice= input("""A. smog
+                 B. clear  """)
+nsert
+print("How do you like your entertainment?")
+choice2= input("""A. Variety
+                 B. Basic  """)
+
+print("How do you like your clothing?")
+choice3= input("""A. Hip
+                 B. Functional """)
+2
+cityscore= 0
+countryscore= 0
+
+if choice == "A":
+    cityscore += 1
+elif choice == "B":
+    countryscore += 1
+    
+choice2= input("How do you like entertainment")
+choice3= input("How do you like hip clothing?")
+
+if choice2 == "A":
+    cityscore += 1
+elif choice2 == "B":
+    countryscore += 1
+
+if choice3 == "A":
+    cityscore += 1
+elif choice3 == "B":
+    countryscore += 1
+
+
---
+++ iftest/condition01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# there was an extra ' on the shebang line that I deleted as well
+
+# create the string hostname
+hostname = "MTG"
+# test logic with the `if` statement
+# what to do if this statement is found to be true
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+
+if "mtg."in hostname:
+    print("The hostname was found to be mtg.")
+
+
+# everything below this line is stuff you should run at the command line, not put inside of your file :)
+
+#python3 iftest/condition01.py
+
+#python3 iftest/condition01.py
+#vim iftest/condition01.py
