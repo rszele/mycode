3c7b5b6b41edaf769a19d3bc043fb5542012d61c
rszele <YOUR@EMAIL.com>
2021-05-16 22:11:14 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,5 @@
+#1/usr/bin/env python3
+
+# create a list called animal-list
+list4 + ["fox", "bear", "dog"]
+print(list4)
---
+++ advlist/list4.py
@@ -0,0 +1,7 @@
+#1/usr/bin/env python3
+
+#create a list called list4
+list4 = ["bear", "dog", "fox"]
+#display list4
+print(list4)
+
