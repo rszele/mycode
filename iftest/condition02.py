5d5d42a8ab38680e3f66a2583cf68a9a82e3a9e0
rszele <YOUR@EMAIL.com>
2021-05-17 03:04:57 +0000

    your commit message

--- iftest/condition02.py
+++ iftest/condition02.py
@@ -2,4 +2,8 @@ vim iftest/condition03.py#!/usr/bin/env python3
 hostname = input("What value should we set for hostname?")
 if hostname == "MTG":
     print("The hostname was found to be mtg")
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
 
